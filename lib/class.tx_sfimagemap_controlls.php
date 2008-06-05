29ea0be810140273a4bb7dda5abe0adb20f88707
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-05 04:00:24 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sf_imagemap/trunk@9851 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_sfimagemap_controlls.php
+++ lib/class.tx_sfimagemap_controlls.php
@@ -1,8 +1,6 @@
 <?php
 
 class tx_sfimagemap_controlls {
-	protected $cObj;
-	
 	protected $table;
 	protected $row;
 	protected $field;
@@ -14,8 +12,6 @@ class tx_sfimagemap_controlls {
 		$this->table = $PA['table'];
 		$this->row = $PA['row'];
 		$this->field = $PA['fieldConf']['config']['field'];
-		
-		$this->cObj = $this->createCObj($this->row['pid']);
 	}
 	
 	public function getSingleField_typePreview($PA, $fObj) {
@@ -23,9 +19,28 @@ class tx_sfimagemap_controlls {
 		
 		$imgPath = $GLOBALS['TCA'][$this->table]['columns'][$this->field]['config']['uploadfolder'] .'/';
 		$imgs = $fObj->extractValuesOnlyFromValueLabelList($this->row[$this->field]);
-
+		$maxW = isset($PA['fieldConf']['config']['maxW']) ? $PA['fieldConf']['config']['maxW'] : 100;
+		$maxH = isset($PA['fieldConf']['config']['maxH']) ? $PA['fieldConf']['config']['maxH'] : 100;
+		
+		$imgObj = t3lib_div::makeInstance('t3lib_stdGraphic');
+		$imgObj->init();
+		$imgObj->mayScaleUp = 0;
+		$imgObj->tempPath = PATH_site . $imgObj->tempPath;
+		
 		$images = array();
 		foreach ($imgs as $imgKey => $imgVal) {
+<<<<<<< .mine
+			$tempImg = $imgObj->imageMagickConvert(PATH_site . $imgPath . $imgVal,
+				'jpg',
+				$maxW . 'm',
+				$maxH . 'm',
+				'',
+				'',
+				'',
+				1);
+			if($tempImg[3] != '') {
+				$images[] = '<img src="'. t3lib_div::resolveBackPath($GLOBALS['BACK_PATH'] . '../' . substr($tempImg[3], strlen(PATH_site))) .'"/>';
+=======
 			$conf = array();
 			
 			$conf['image.']['file'] = $imgVal;
@@ -35,7 +50,10 @@ class tx_sfimagemap_controlls {
 			
 			if (isset($PA['fieldConf']['config']['maxW'])) {
 				$conf['image.']['file.']['maxW'] = $PA['fieldConf']['config']['maxW'];
+>>>>>>> .r336
 			}
+<<<<<<< .mine
+=======
 			if (isset($PA['fieldConf']['config']['maxH'])) {
 				$conf['image.']['file.']['maxH'] = $PA['fieldConf']['config']['maxH'];
 			}
@@ -45,30 +63,16 @@ class tx_sfimagemap_controlls {
 				$images[] = $this->cObj->TEXT(array('value' => $conf['image.']['file'], 'wrap' => '<b>|</b>'));
 				$errorcount++;
 			}
+>>>>>>> .r336
 		}
+<<<<<<< .mine
+
+		return implode('', $images);
+=======
 		
 		return implode('<br/>', $images);
+>>>>>>> .r336
 	}
-	
-	protected function createCObj($pid = 1){
-		require_once(PATH_site . 'typo3/sysext/cms/tslib/class.tslib_fe.php');
-		require_once(PATH_site . 'typo3/sysext/cms/tslib/class.tslib_feuserauth.php');
-		require_once(PATH_site . 'typo3/sysext/cms/tslib/class.tslib_content.php') ;
-
-			// Finds the TSFE classname
-		$TSFEclassName = t3lib_div::makeInstanceClassName('tslib_fe');
- 
-			// Create the TSFE class.
-		$GLOBALS['TSFE'] = new $TSFEclassName($GLOBALS['TYPO3_CONF_VARS'], $pid, '0', 0, '', '', '', '');
-
-		$GLOBALS['TSFE']->initFEuser();
- 
-		$GLOBALS['TSFE']->initTemplate();
-
-		$cObj = t3lib_div::makeInstance('tslib_cObj');
-		return $cObj;
-	}
-	
 }
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/sf_imagemap/lib/class.tx_sfimagemap_controlls.php'])	{
