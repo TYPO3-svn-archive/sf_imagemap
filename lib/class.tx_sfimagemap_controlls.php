4a369365107d9816ad7d2f78051e0bf153a4457f
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-05 20:13:53 +0000

    Cleaning up control code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sf_imagemap/trunk@9867 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_sfimagemap_controlls.php
+++ lib/class.tx_sfimagemap_controlls.php
@@ -4,47 +4,51 @@ class tx_sfimagemap_controlls {
 	protected $table;
 	protected $row;
 	protected $conf;
-	
-	protected $imageField;
-	protected $widthField;
-	protected $heightField;
+	protected $iObj;
 	
 	public function __construct() {
+		$this->iObj = t3lib_div::makeInstance('t3lib_stdGraphic');
+		$this->iObj->init();
+		$this->iObj->mayScaleUp = 0;
+		$this->iObj->tempPath = PATH_site . $this->iObj->tempPath;
 	}
 	
 	private function init($PA, $fObj) {
 		$this->table = $PA['table'];
 		$this->row = $PA['row'];
 		$this->conf = $PA['fieldConf']['config'];
-		$this->widthField = $PA['fieldConf']['config']['widthField'];
-		$this->heightField = $PA['fieldConf']['config']['heightField'];
 	}
 	
+	/**
+	 * Import ext_tables.sql statements
+	 *
+	 * @param array		$PA Array containing configurations for record
+	 * @param object	$fObj TCEform object
+	 *
+	 * @return string	$images String with Preview image tags
+	 */
 	public function getSingleField_typePreview($PA, $fObj) {
 		$this->init($PA, $fObj);
 		
 		$imgPath = $GLOBALS['TCA'][$this->table]['columns'][$this->conf['imageField']]['config']['uploadfolder'] .'/';
 		$imgs = $fObj->extractValuesOnlyFromValueLabelList($this->row[$this->conf['imageField']]);
 		
-		if (isset($this->row[$this->conf['widthField']]) && $this->row[$this->conf['widthField']] > 0) {
+		if (isset($this->row[$this->conf['widthField']])
+			&& $this->row[$this->conf['widthField']] > 0) {
 			$maxW = $this->row[$this->conf['widthField']]; 
 		} else {
 			$maxW = 100;
 		}
-		if (isset($this->row[$this->conf['heightField']]) && $this->row[$this->conf['heightField']] > 0) {
+		if (isset($this->row[$this->conf['heightField']])
+			&& $this->row[$this->conf['heightField']] > 0) {
 			$maxH = $this->row[$this->conf['heightField']]; 
 		} else {
 			$maxH = 100;
 		}
-
-		$imgObj = t3lib_div::makeInstance('t3lib_stdGraphic');
-		$imgObj->init();
-		$imgObj->mayScaleUp = 0;
-		$imgObj->tempPath = PATH_site . $imgObj->tempPath;
 		
 		$images = array();
 		foreach ($imgs as $imgKey => $imgVal) {
-			$tempImg = $imgObj->imageMagickConvert(PATH_site . $imgPath . $imgVal,
+			$tempImg = $this->iObj->imageMagickConvert(PATH_site . $imgPath . $imgVal,
 				'jpg',
 				$maxW . 'm',
 				$maxH . 'm',
@@ -56,7 +60,7 @@ class tx_sfimagemap_controlls {
 				$images[] = '<img src="'. t3lib_div::resolveBackPath($GLOBALS['BACK_PATH'] . '../' . substr($tempImg[3], strlen(PATH_site))) .'"/>';
 			}
 		}
-;
+
 		return implode('', $images);
 	}
 }
